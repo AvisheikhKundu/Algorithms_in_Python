a46f1b6a73d5f829b49be4257e5ea5e3db2f5bfd
Avisheikh Kundu <avisheikh76@gmail.com>
2023-09-11 16:43:15 +0600

    new code added into MergeSort.py

--- MergeSort.py
+++ MergeSort.py
@@ -1,21 +1,36 @@
-def mergesort(arr):
-    if len(arr)<=1:
+def merge_sort(arr):
+    if len(arr) <= 1:
         return arr
-    mid = len(arr)//2
-    left = arr[:mid]
-    right = arr[mid:]
-    sortedleft = mergesort(left)
-    sortedright = mergesort(right)
-    i, j = 0, 0
-    sortedarr = []
-    while(i < len(sortedleft)) and j < len(sortedright):
-        if sortedleft[i] < sortedright[j]:
-            sortedarr.append(sortedleft[i])
-            i += 1
-        else:
-            sortedarr.append(sortedright[j])
-            j += 1
 
-    return sortedarr + sortedleft[i:] + sortedright[j:]
-a = [2, 8, 3, 5]
-print (mergesort(a))
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    result = []
+    left_index, right_index = 0, 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+
+    result.extend(left[left_index:])
+    result.extend(right[right_index:])
+
+    return result
+
+# Input from the user
+user_input = input("Enter a list of numbers separated by spaces: ")
+input_list = [int(x) for x in user_input.split()]
+
+sorted_list = merge_sort(input_list)
+print("Sorted list:", sorted_list)
