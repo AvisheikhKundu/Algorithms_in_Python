57a06e23e33dd90afcc5a46da6293b1adf1e7f6e
Avisheikh Kundu <avisheikh76@gmail.com>
2023-09-11 13:17:07 +0600

    new code added into Linear_Search.py

--- Linear_Search.py
+++ Linear_Search.py
@@ -5,9 +5,16 @@ def linear_search(arr, target):
     return -1  # Return -1 if target is not found in the array
 
 # Example usage
-arr = [3, 9, 4, 2, 7, 1]
-target = 7
-result = linear_search(arr, target)
+input_str = input("Enter a list of numbers separated by spaces: ")
+input_list = [int(x) for x in input_str.split()]
+
+# Prompt the user to enter the target
+target = int(input("Enter the target number to search for: "))
+
+result = linear_search(input_list, target)  # Assign the result of linear_search to 'result'
+
+# Output: Sorted list
+print("Sorted list:", input_list)
 
 if result != -1:
     print(f"Target {target} found at index {result}")
