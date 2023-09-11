d807e26e1a076fdc0a95df71bca16a7efb922d87
Avisheikh Kundu <avisheikh76@gmail.com>
2023-09-11 13:22:09 +0600

    new code added into Binary_Search.py

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,23 +1,39 @@
 def binary_search(arr, target):
-    left, right = 0, len(arr) - 1
+    """
+    Binary search algorithm to find the position of the target element in the given sorted list.
+
+    Parameters:
+    arr (list): The sorted list to search through.
+    target: The element to find in the list.
+
+    Returns:
+    int: The index of the target element if found, otherwise -1.
+    """
+    left = 0
+    right = len(arr) - 1
 
     while left <= right:
-        mid = left + (right - left) // 2
+        mid = (left + right) // 2
 
         if arr[mid] == target:
-            return mid
+            return mid  # Return the index where the target is found
         elif arr[mid] < target:
             left = mid + 1
         else:
             right = mid - 1
 
-    return -1
+    return -1  # Return -1 if target is not found in the array
 
-arr = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-target = 11
-result = binary_search(arr, target)
+# Example usage
+input_str = input("Enter a sorted list of numbers separated by spaces: ")
+input_list = [int(x) for x in input_str.split()]
+
+# Prompt the user to enter the target
+target = int(input("Enter the target number to search for: "))
+
+result = binary_search(input_list, target)  # Assign the result of binary_search to 'result'
 
 if result != -1:
-    print(f"Element found at index {result}")
+    print(f"Target {target} found at index {result}")
 else:
-    print("Element not found")
+    print("Target not found in the array")
