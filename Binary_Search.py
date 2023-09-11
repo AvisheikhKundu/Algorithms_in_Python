8878aef21bd93bdd9a84e51cd05a49d95a8950da
Avisheikh Kundu <avisheikh76@gmail.com>
2023-09-11 12:12:16 +0600

    new code added into Bubble_Sort.py

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,26 +1,4 @@
-def binary_search(numbers,number_to_find):
-    low=0
-    high=len(numbers)-1
-    while(low<=high):
-        mid=low+(high-low)//2
-        if(numbers[mid]==number_to_find):
-            return mid
-        elif (numbers[mid]<number_to_find):
-            low=mid+1
-        else :
-            high=mid-1
-    return -1
-a=list(map(int, input("elements of array:-").strip().split()))
-n=int(input())
-if (binary_search(a,n)==-1):
-    print("Not found")
-else :
-    print("Found in index",str(binary_search(a,n)))
-
-
-#AN_EXAMPLE:
-
-    def binary_search(arr, target):
+def binary_search(arr, target):
     left, right = 0, len(arr) - 1
 
     while left <= right:
--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -2,13 +2,19 @@ def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
         swapped = False
-        for j in range(0, n - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
                 swapped = True
         if not swapped:
             break
 
-arr = [64, 34, 25, 12, 22, 11, 90]
-bubble_sort(arr)
-print("Sorted array:", arr)
+# Input from the user
+input_str = input("Enter a list of numbers separated by spaces: ")
+input_list = [int(x) for x in input_str.split()]
+
+# Call the bubble_sort function to sort the input list
+bubble_sort(input_list)
+
+# Print the sorted list
+print("Sorted list:", input_list)
